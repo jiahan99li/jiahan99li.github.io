5a582c97863799adf6ace0bba4bccb4ea0b4473c
Vinny Li <jiahanlijiang@gmail.com>
2024-04-27 14:58:44 -0500

    Add animation to About Page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,14 @@
 import './About.css';
+import profile from '../../assets/profile.png';
 
 function About() {
   return (
     <div className="About">
       <div className='aboutIntro'>
         <div className='introLeft'>
-
+          <div className='introImageContainer'>
+            <img src={profile} alt='Profile' />
+          </div>
         </div>
         <div className='introRight'>
           <h1>About</h1>
