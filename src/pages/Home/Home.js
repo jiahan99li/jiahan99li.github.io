0de37c57c40076969e655588019d96ebce0e7bc9
Vinny Li <jiahanlijiang@gmail.com>
2024-04-26 22:44:49 -0500

    Updates on Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,17 @@ function Home() {
     <div className="Home">
       <h1>Hey!I'm Vinny</h1>
       <h2>Software Developer & Salesforce Developer</h2>
+      <div className="homeDescription">
+        <div className='descriptionLeft'>
+          <p>Currently a Solution Analyst working on mobile app and salesforce development at Deloitte.</p>
+        </div>
+        <div className='descriptionRight'>
+          <p>Experienced in either professional or personal level on Flutter, React, HTML, CSS, PHP and more.</p>
+        </div>
+      </div>
+      <div className='homeSocials'>
+
+      </div>
     </div>
   );
 }
