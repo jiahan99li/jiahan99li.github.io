c855718906ac0e322cc08dda446147f477c84ee4
Vinny Li <jiahanlijiang@gmail.com>
2025-06-28 11:43:02 -0500

    Final Updates 06/25

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,4 +1,5 @@
 import './Projects.css'
+import { Link, useResolvedPath, useMatch } from 'react-router-dom'
 import React from "react"
 
 function Projects() {
@@ -15,20 +16,24 @@ function Projects() {
             <h2 className="uppercase font-bold text-[28px] mb-8">
               Social Media App
             </h2>
-            <p className="text-sm leading-relaxed mb-12">
+            <p className="text-sm leading-relaxed mb-0">
               Social Media App developed using Flutter, featuring real-time chat and posts to share with friends.
             </p>
-            <a style={{ color: purple }} href="/socialmedia" className="w-fit px-6 py-3 bg-white rounded-full hover:ring-1 hover:!text-white hover:bg-inherit hover:ring-white transition-all mt-auto learnmore">Learn more</a>
+            <div className='mt-auto'>
+              <CustomLink to="/socialmedia">Learn more</CustomLink>
+            </div>
           </section>
           {/* Section 2 */}
-          <section id="wissApp" className="h-[380px] py-8 px-10 rounded-md overflow-hidden flex flex-col">
+          <section id="wissApp" className="h-[380px] py-8 px-10 rounded-md overflow-hidden flex flex-col flex-1">
             <h2 className="uppercase font-bold text-[28px] mb-8">
               Wiss App
             </h2>
-            <p className="text-sm leading-relaxed mb-12">
+            <p className="text-sm leading-relaxed mb-0">
               Cross Platform tools app released on Android and iOS, featuring unit exchange, calculator, and more.
             </p>
-            <a style={{ color: purple }} href="/wiss" className="w-fit px-6 py-3 bg-white rounded-full hover:ring-1 hover:!text-white hover:bg-inherit hover:ring-white transition-all mt-auto learnmore">Learn more</a>
+            <div className='mt-auto'>
+              <CustomLink to="/wiss">Learn more</CustomLink>
+            </div>
           </section>
         </div>
       </div>
@@ -37,3 +42,14 @@ function Projects() {
 }
 
 export default Projects;
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li style={{ listStyleType: 'none' }} className={isActive ? "active" : ""}>
+      <Link to={to} {...props} style={{ color: "hsl(261 39.3% 43.9%)" }} className='w-fit px-6 py-3 bg-white rounded-full hover:ring-1 hover:!text-white hover:bg-inherit hover:ring-white transition-all learnmore'>{children}</Link>
+    </li>
+  )
+}
